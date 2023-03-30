15108eaaa913da997198ad1a66078f713a3d927a
mihailomajstorovic47 <73445435+mihailomajstorovic47@users.noreply.github.com>
2023-03-31 00:28:55 +0200

    new undersampled dataset for neural network
    data normalization done, too

--- pandas_csv_service.py
+++ pandas_csv_service.py
@@ -8,14 +8,16 @@ from sklearn.preprocessing import MinMaxScaler
 import math
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegression, LinearRegression
 from yellowbrick.model_selection import FeatureImportances # Correlation finding
 from imblearn.over_sampling import SMOTE
 from imblearn.over_sampling import RandomOverSampler
+from collections import Counter
 
 tracks = pd.read_csv('tracks.csv')
 artists = pd.read_csv('artists.csv')
 combined = pd.read_csv('merged-20000.csv')
+final_tracks = pd.read_csv('final-shuffled-20000.csv')
 
 def find_most_popular_artist(artist_ids):
     max_popularity = 0
@@ -65,7 +67,7 @@ def merge_tracks_and_artists():
     time_signature = []
     artist_popularity = []     #lista koja ce predstavljati novu kolonu dataseta
     popularity_class = []       #pomocna lista koja ce sluziti za oversampling: klasa 1 - popularnost(0-20), klasa 2 - popularnost(21-40)...
-    shuffled_tracks = tracks.sample(frac=1)
+    shuffled_tracks = final_tracks.sample(frac=1)
     zero_to_ten = 0
     eleven_to_twenty = 0
     twenty_one_to_thirty = 0
@@ -107,65 +109,66 @@ def merge_tracks_and_artists():
         print(popular_counter)
     previous_counter = counter
     for song in shuffled_tracks.values:
-        if counter >= 19200:
+        if counter >= 5888:
             break
-        song_popularity = song[2]
-        if 0 <= song_popularity < 11 and zero_to_ten < 2400:
+        song_popularity = song[3]
+        if 0 <= song_popularity < 11 and zero_to_ten < 736:
             zero_to_ten += 1
             popularity_class.append(1)
             counter += 1
-        elif 11 <= song_popularity < 21 and eleven_to_twenty < 2400:
+        elif 11 <= song_popularity < 21 and eleven_to_twenty < 736:
             eleven_to_twenty += 1
             popularity_class.append(1)
             counter += 1
-        elif 21 <= song_popularity < 31 and twenty_one_to_thirty < 2400:
+        elif 21 <= song_popularity < 31 and twenty_one_to_thirty < 736:
             twenty_one_to_thirty += 1
             popularity_class.append(2)
             counter += 1
-        elif 31 <= song_popularity < 41 and thirty_one_to_forty < 2400:
+        elif 31 <= song_popularity < 41 and thirty_one_to_forty < 736:
             thirty_one_to_forty += 1
             popularity_class.append(2)
             counter += 1
-        elif 41 <= song_popularity < 51 and forty_one_to_fifty < 2400:
+        elif 41 <= song_popularity < 51 and forty_one_to_fifty < 736:
             forty_one_to_fifty += 1
             popularity_class.append(3)
             counter += 1
-        elif 51 <= song_popularity < 61 and fifty_one_to_sixty < 2400:
+        elif 51 <= song_popularity < 61 and fifty_one_to_sixty < 736:
             fifty_one_to_sixty += 1
             popularity_class.append(3)
             counter += 1
-        elif 61 <= song_popularity < 71 and sixty_one_to_seventy < 2400:
+        elif 61 <= song_popularity < 71 and sixty_one_to_seventy < 736:
             sixty_one_to_seventy += 1
             popularity_class.append(4)
             counter += 1
-        elif 71 <= song_popularity < 81 and seventy_one_to_eighty < 2400:
+        elif 71 <= song_popularity < 81 and seventy_one_to_eighty < 736:
             seventy_one_to_eighty += 1
             popularity_class.append(4)
             counter += 1
         if previous_counter != counter:
-            id.append(song[0])
-            name.append(song[1])
-            popularity.append(song[2])
-            duration_ms.append(song[3])
-            explicit.append(song[4])
-            artists.append(song[5])
-            id_artists.append(song[6])
-            release_date.append(song[7])
-            danceability.append(song[8])
-            energy.append(song[9])
-            key.append(song[10])
-            loudness.append(song[11])
-            mode.append(song[12])
-            speechiness.append(song[13])
-            acousticness.append(song[14])
-            instrumentalness.append(song[15])
-            liveness.append(song[16])
-            valence.append(song[17])
-            tempo.append(song[18])
-            time_signature.append(song[19])
-            artist_popularity.append(find_most_popular_artist(song[6][1:len(song[6]) - 1]))        #nalazenje popularnosti umetnika
+            id.append(song[1])
+            name.append(song[2])
+            popularity.append(song[3])
+            duration_ms.append(song[4])
+            explicit.append(song[5])
+            artists.append(song[6])
+            id_artists.append(song[7])
+            release_date.append(str(song[8]))
+            danceability.append(song[9])
+            energy.append(song[10])
+            key.append(song[11])
+            loudness.append(song[12])
+            mode.append(song[13])
+            speechiness.append(song[14])
+            acousticness.append(song[15])
+            instrumentalness.append(song[16])
+            liveness.append(song[17])
+            valence.append(song[18])
+            tempo.append(song[19])
+            time_signature.append(song[20])
+            artist_popularity.append(song[21])        #nalazenje popularnosti umetnika
             print(counter)
             previous_counter = counter
+    print(release_date)
     merged.insert(0, "id", id, allow_duplicates=True)
     merged.insert(1, "name", name, allow_duplicates=True)
     merged.insert(2, "popularity", popularity, allow_duplicates=True)
@@ -188,10 +191,12 @@ def merge_tracks_and_artists():
     merged.insert(19, "time_signature", time_signature, allow_duplicates=True)
     merged.insert(20, "artist_popularity", artist_popularity, allow_duplicates=True)
     merged.insert(21, "popularity_class", popularity_class, allow_duplicates=True)
+    print(merged.release_date)
     merged.release_date = merged.release_date.str[0:4]      #pretvaranje datuma objave pesme u godinu, zbog lakse obrade podataka
-    merged.to_csv('final-20000.csv')     #export dataseta pesama zajedno sa popularnosti izvodjaca u .csv fajl
+    print(merged.release_date)
+    merged.to_csv('undersampled-20000.csv')     #export dataseta pesama zajedno sa popularnosti izvodjaca u .csv fajl
     merged_shuffled = merged.sample(frac=1)
-    merged_shuffled.to_csv('final-shuffled-20000.csv')     #export dataseta pesama zajedno sa popularnosti izvodjaca u .csv fajl
+    merged_shuffled.to_csv('undersampled-shuffled-20000.csv')     #export dataseta pesama zajedno sa popularnosti izvodjaca u .csv fajl
     #print(artists_popularity)
     #print(merged.values)
 
@@ -212,26 +217,38 @@ def show_importances():
     feature_importances.fit(train_X, train_y)
     feature_importances.show()
 
-def logistic_regression_prediction():
-    df = pd.read_csv('merged-5000.csv')
+def linear_regression_prediction():
+    df = pd.read_csv('undersampled-shuffled-20000.csv')
     y = df.popularity
     features = ['duration_ms', 'explicit', 'release_date',
                 'danceability', 'energy', 'key', 'loudness', 'mode',
                 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence',
                 'tempo', 'time_signature', 'artist_popularity']
     X = df[features]
-    print(X.head())
-    smote = SMOTE()
+    X['duration_ms'] = (X['duration_ms'] - X['duration_ms'].min()) / (X['duration_ms'].max() - X['duration_ms'].min())
+    X['release_date'] = (X['release_date'] - X['release_date'].min()) / (X['release_date'].max() - X['release_date'].min())
+    X['key'] = (X['key'] - X['key'].min()) / (X['key'].max() - X['key'].min())
+    X['tempo'] = (X['tempo'] - X['tempo'].min()) / (X['tempo'].max() - X['tempo'].min())
+    X['time_signature'] = (X['time_signature'] - X['time_signature'].min()) / (X['time_signature'].max() - X['time_signature'].min())
+    X['speechiness'] = (X['speechiness'] - X['speechiness'].min()) / (X['speechiness'].max() - X['speechiness'].min())
+    X['loudness'] = X['loudness'] + X['loudness'].min()
+    X['loudness'] = (X['loudness'] - X['loudness'].min()) / (X['loudness'].max() - X['loudness'].min())
+    X['artist_popularity'] = (X['artist_popularity'] - X['artist_popularity'].min())/(X['artist_popularity'].max() - X['artist_popularity'].min())
+    print(X)
+    print(X.info())
     train_X, test_X, train_y, test_y = train_test_split(X, y, test_size=0.2, random_state=0)
-    #train_X, train_y = smote.fit_resample(train_X, train_y)
-    lr_model = LogisticRegression(random_state=0)
+    lr_model = LinearRegression()
     lr_model.fit(train_X, train_y)
     val_preds1 = lr_model.predict(test_X)
-    error = mean_squared_error(test_y, val_preds1)
-    print(f'Mean squared error of this model: {math.sqrt(error):.3f}')
+    mse_error = mean_squared_error(test_y, val_preds1)
+    mae_error = mean_absolute_error(test_y, val_preds1)
+    r2 = r2_score(test_y, val_preds1)
+    print(f' Root mean squared error of this model: {math.sqrt(mse_error):.3f}')
+    print(f' Mean absolute error of this model: {mae_error:.3f}')
+    print(f' R2 score of this model: {r2 * 100:.3f} %')
 
 def random_forest_prediction():
-    df = pd.read_csv('merged-20000.csv')
+    df = pd.read_csv('undersampled-shuffled-20000.csv')
     print(df.info())
     y = df.popularity
     features = ['duration_ms', 'explicit', 'release_date',
